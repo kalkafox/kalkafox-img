0266dcbd259ff454027b975e1edef067715c5a62
Kalka <kalka2088@gmail.com>
2023-03-26 20:41:05 -0400

    use start script instead

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
-
-cargo build --release
-
 # Set working directory to frontend/
 cd frontend
 
@@ -14,3 +11,5 @@ npm run build
 
 # Set working directory to ../
 cd ..
+
+bin/kalkafox-img
